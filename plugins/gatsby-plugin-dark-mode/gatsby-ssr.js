4cb777c8ecb27d49a2763550f058352a34faeed7
Shahed Nasser <shahednasser@gmail.com>
2021-02-04 13:38:31 +0000

    remove submodule

---
+++ plugins/gatsby-plugin-dark-mode/gatsby-ssr.js
@@ -0,0 +1,43 @@
+const React = require('react')
+
+exports.onRenderBody = function({ setPreBodyComponents }) {
+  setPreBodyComponents([
+    React.createElement('script', {
+      key: 'gatsby-plugin-dark-mode',
+      dangerouslySetInnerHTML: {
+        __html: `
+void function() {
+  window.__onThemeChange = function() {}
+
+  var preferredTheme
+  try {
+    preferredTheme = localStorage.getItem('theme')
+  } catch (err) { }
+
+  function setTheme(newTheme) {
+    document.body.setAttribute('data-theme', newTheme);
+
+    window.__theme = newTheme
+    preferredTheme = newTheme
+    window.__onThemeChange(newTheme)
+  }
+
+  window.__setPreferredTheme = function(newTheme) {
+    setTheme(newTheme)
+    try {
+      localStorage.setItem('theme', newTheme)
+    } catch (err) {}
+  }
+
+  var darkQuery = window.matchMedia('(prefers-color-scheme: dark)')
+  darkQuery.addListener(function(e) {
+    window.__setPreferredTheme(e.matches ? 'dark' : 'light')
+  })
+
+  setTheme(preferredTheme || (darkQuery.matches ? 'dark' : 'light'))
+}()
+    `,
+      },
+    }),
+  ])
+}
